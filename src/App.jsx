fb9d6bd91b6dab57634c5db1a71f306e88055926
Marx Alonso <marxchip99@gmail.com>
2024-07-30 18:13:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ButtonGradient from './assets/svg/ButtonGradient'
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
