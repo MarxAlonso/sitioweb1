533392f000b4f66cfb5135223d5a23a83ec57896
Marx Alonso <marxchip99@gmail.com>
2024-07-30 21:15:20 -0500

    agregando header y diseños

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import ButtonGradient from './assets/svg/ButtonGradient'
-import Button from './components/Button';
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
-      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-        <Button className="mt-10">
-          Something
-        </Button>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
       </div>
       <ButtonGradient />
     </>
